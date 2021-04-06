8f87c5884f2c69f5974c07f1e7db78dbb51ac44f
Tevinthuku <tevinthuku@gmail.com>
2021-04-06 23:21:18 +0300

    parse numbers & strings

--- src/parser.rs
+++ src/parser.rs
@@ -1,13 +1,25 @@
+use crate::ast;
 use crate::token::{Token, TokenWithContext};
 use std::{iter::Peekable, slice::Iter};
 
-pub fn parse(tokens: &[TokenWithContext]) {
-    // let mut results = vec![];
+pub fn parse(tokens: &[TokenWithContext]) -> std::vec::Vec<ast::JSON> {
+    let mut results: Vec<ast::JSON> = vec![];
     let mut peekable_tokens = tokens.iter().peekable();
-    skip_initial_empty_lines(&mut peekable_tokens);
+    skip_initial_new_lines(&mut peekable_tokens);
+
+    while let Some(token) = peekable_tokens.peek() {
+        let element = match &token.token {
+            Token::DigitLiteral(_literal) => ast::JSON::NumberType,
+            Token::StringLiteral(_literal) => ast::JSON::StringType,
+            _ => todo!(),
+        };
+        results.push(element);
+    }
+
+    results
 }
 
-fn skip_initial_empty_lines(peekable_tokens: &mut Peekable<Iter<TokenWithContext>>) {
+fn skip_initial_new_lines(peekable_tokens: &mut Peekable<Iter<TokenWithContext>>) {
     while let Some(token) = peekable_tokens.peek() {
         match token.token {
             Token::NextLine => {
